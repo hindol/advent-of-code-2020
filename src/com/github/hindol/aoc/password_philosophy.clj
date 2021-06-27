87f8aafc526734cbb80de462bb30c3b6bee42bd3
Hindol Adhya <705227+hindol@users.noreply.github.com>
2021-06-27 22:39:17 +0530

    Day 2

---
+++ src/com/github/hindol/aoc/password_philosophy.clj
@@ -0,0 +1,79 @@
+(ns com.github.hindol.aoc.password-philosophy
+  (:require [clojure.java.io :as io]))
+
+(def input-file
+  "resources/password-philosophy.txt")
+
+(def line-regex
+  #"(\d+)-(\d+) (\w): (.*)")
+
+(defn str->int
+  [s]
+  (Integer/parseInt s))
+
+(defn count-if
+  [pred coll]
+  (count (filter pred coll)))
+
+(defn get-many
+  "Returns the values corresponding to `keys` from map `m`. If `m` is
+  sequential, `keys` are assumed to be indices."
+  [m keys]
+  (for [k keys]
+    (get m k)))
+
+(defn seqify
+  "Returns a sequence if `thing` is not sequential, else the `thing` itself."
+  [thing]
+  (if (sequential? thing)
+    thing
+    [thing]))
+
+(defn update-values
+  [update-fns m]
+  (reduce-kv (fn [m k v]
+               (if-let [f (get update-fns k)]
+                 (assoc m k (f v))
+                 (assoc m k v)))
+             {}
+             m))
+
+(defn parse-line
+  [re line]
+  (->> line
+       (re-matches re)
+       rest))
+
+(def input-items
+  (let [lines (-> input-file io/reader line-seq)]
+    (->> lines
+         (map #(->> %
+                    (parse-line line-regex)
+                    (zipmap [:x :y :letter :password])
+                    (update-values {:x str->int
+                                    :y str->int
+                                    :letter first}))))))
+
+(defn valid-password?
+  "Returns true if the password conforms to the password policies. Each policy
+  must be a predicate function which can be applied to the password."
+  [policies password]
+  (let [policies (seqify policies)
+        combined-policy (apply every-pred policies)]
+    (combined-policy password)))
+
+(defn make-first-policy
+  [{:keys [x y letter]}]
+  #(<= x (count (filter #{letter} %)) y))
+
+(defn make-second-policy
+  [{:keys [x y letter]}]
+  #(= 1 (count-if #{letter} (get-many % [(dec x) (dec y)]))))
+
+(comment
+  (count-if true?
+            (for [item input-items]
+              (valid-password? (make-first-policy item) (:password item))))
+  (count-if true?
+            (for [item input-items]
+              (valid-password? (make-second-policy item) (:password item)))))
